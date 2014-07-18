47c862507a21a8664e82b3fa80500425fbf8f9ef
Adam Joseph Cook <acook@alliedstrand.com>
2014-07-18 16:51:40 -0500

    chore(tool): added project build tooling and build documentation

--- README.md
+++ README.md
@@ -2,3 +2,32 @@ graph-paper
 ===========
 
 Web component for generating various types of engineering graph paper using SVG, written in Dart and Polymer.
+
+## Overview
+
+TODO(adamjcook): Add content here.
+   
+## Status
+
+Please note that this is a beta release of this package and API changes are very probable while this package is in beta.
+
+## Using Elements
+
+All elements live at the top-level of the `lib/` folder.
+
+Import into HTML:
+
+    TODO(adamjcook): Add content here.
+
+Import into Dart:
+
+    TODO(adamjcook): Add content here.
+    
+## Build Notes
+
+Use the `update.dart` script to build the import .html and .dart files based on the sources in the `lib/src` directory.
+The `update.dart` tool expects to be run in the repository root.
+
+To build this project, run the following command in the repository root of the project:
+
+    `./tool/update.dart lib/src/graph_paper/graph_paper.html`
---
+++ tool/update.dart
@@ -0,0 +1,27 @@
+#!/usr/bin/env dart
+// Copyright (c) 2014 The Polymer Project Authors. All rights reserved.
+// This code may only be used under the BSD style license found at http://polymer.github.io/LICENSE.txt
+// The complete set of authors may be found at http://polymer.github.io/AUTHORS.txt
+// The complete set of contributors may be found at http://polymer.github.io/CONTRIBUTORS.txt
+// Code distributed by Google as part of the polymer project is also
+// subject to an additional IP rights grant found at http://polymer.github.io/PATENTS.txt
+
+import 'dart:io';
+import 'package:path/path.dart' as path;
+import 'package:graph_paper/generate_dart_api.dart' as generator;
+
+main(args) {
+  generator.main(args);
+}
+
+const String _POLYMER_HTML_FORWARD = '''
+<!--
+Copyright (c) 2014 Adam Joseph Cook. All rights reserved.
+This code may only be used under The MIT License found at http://adamjcook.github.io/graph-paper/LICENSE
+The complete set of contributors may be found at http://adamjcook.github.io/graph-paper/CONTRIBUTORS
+-->
+
+<!-- Dart note: load polymer for Dart and JS from the same place -->
+<link rel="import" href="../../../../packages/polymer/polymer.html">
+${generator.EMPTY_SCRIPT_WORKAROUND_ISSUE_11}
+''';
