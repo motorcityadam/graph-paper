612cfe5ebd2fcb2b95985a584264c98e4f340533
Adam Joseph Cook <acook@alliedstrand.com>
2014-07-18 16:57:56 -0500

    feat(graph-paper): copied web component HTML template and Dart file from workbook project to this project

---
+++ lib/src/graph_paper/graph_paper.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2014 Adam Joseph Cook. All rights reserved.
+// This code may only be used under The MIT License found at http://adamjcook.github.io/graph-paper/LICENSE
+// The complete set of contributors may be found at http://adamjcook.github.io/graph-paper/CONTRIBUTORS
+
+library graph_paper;
+
+import 'dart:async';
+import 'dart:html';
+
+import 'package:polymer/polymer.dart';
+
+/**
+ * TODO(adamjcook): add comments for this element
+ */
+@CustomTag('graph-paper')
+class UtilGraphPaper extends PolymerElement {
+
+  UtilGraphPaper.created() : super.created() {}
+
+}
